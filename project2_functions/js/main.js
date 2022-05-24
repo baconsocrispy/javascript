0d4464a5ae3b463cce27093d742bb1df11d487ce
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-05-24 11:48:06 -0700

    Added project 2 files

---
+++ project2_functions/js/main.js
@@ -0,0 +1,13 @@
+// Function assignment
+function myFunc1(text1, text2)  {
+  var var1 = text1,  var2 = text2
+  document.getElementById("par_id_1").innerHTML = var1 + " and " + var2
+}
+
+// Operator assignment
+function myFunc2(text1, text2) {
+  var var1 = text1, var2 = text2
+  document.getElementById("par_id_2").innerHTML += " " + var1 + " and " + var2
+}
+
+
