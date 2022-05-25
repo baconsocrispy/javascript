afa6d41cce308608d8a3b89d9137af16085529ec
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-05-24 18:00:32 -0700

    Added project 7 files

---
+++ basic_js_projects/project7_scope_time_function/js/main.js
@@ -0,0 +1,15 @@
+// write a global variable
+var global_var
+
+// write a local variable
+function localVar() {
+  var local = 72
+  return local * 100
+}
+
+// intentional error
+// function badFunc() {
+//   var = local 
+//   return local + 100
+// }
+
